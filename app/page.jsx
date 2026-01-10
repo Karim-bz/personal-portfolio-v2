c03f5a72abd1ed06585ef3578ab89bfa35970c7e
Karim Bouzid <kaarimbouzid@gmail.com>
2026-01-10 15:00:19 +0100

    configure tailwindcss and project files

---
+++ app/page.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-2xl font-bold">Welcome to Next.js!</h1>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+  },
+}
