5690eebdaab56e30e94f1c00f89601ea0209a0df
Karim Bouzid <kaarimbouzid@gmail.com>
2026-01-11 00:49:53 +0100

    working on home page UI

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,47 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+import Social from "../components/Social";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span>Software Engineer</span>
+            <h1 className="h1">
+              Hello I'm <br /> <span>Karim Bouzid</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and I am
+              proficient in various programming languages and technologies.
+            </p>
+            {/* btn and socials */}
+            <div>
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full 
+                   flex justify-center items-center text-accent text-base
+                   hover:bg-accent hover:text-primary 
+                   hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
